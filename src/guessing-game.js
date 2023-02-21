ba234fbc8b6a7eeb0a0d1ea34e05fd11bb42bb2b
M-Rasul <rasulmamashev2@gmail.com>
2023-02-21 18:30:25 +0500

    init: create guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.res = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.res = Math.ceil((this.min + this.max) / 2);
+        return this.res;
     }
 
     lower() {
-
+        this.max = this.res;
     }
 
     greater() {
-
+        this.min = this.res;
     }
 }
 
